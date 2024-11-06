f3dbda24f0314e8311484d13ad56df4749098113
Nessatunes <essatunes@gmail.com>
2024-11-06 11:00:29 -0300

    feat: add authentication clerk and husky

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,16 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const { userId } = await auth();
+  if (userId) {
+    redirect("/");
+  }
+
   return (
     <div className="grid h-full grid-cols-2">
       {/* Esquerda */}
@@ -15,15 +23,17 @@ const LoginPage = () => {
           className="mb-8"
         />
         <h1 className="mb-3 text-4xl font-bold">Bem Vindo!</h1>
-        <p className="text-muted-foreground mb-8">
+        <p className="mb-8 text-muted-foreground">
           A Finance Ai é uma plataforma de gestão financeira que ultiliza IA
           para monitorar suas movimentações e oferecer insights personalizados,
           facilitando o controle do seu orçamento
         </p>
-        <Button variant="outline">
-          <LogInIcon className="mr-2" />
-          Fazer login ou criar conta
-        </Button>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
 
       {/* Direita */}
